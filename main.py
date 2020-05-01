b43f15f809d071f1e2e9f6f95ec551f14bfac52a
Douglas Pompermayer <contatoinfomega@gmail.com>
2020-05-01 21:55:37 +0000

    teste

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Doug_Master(BaseModel):
+    canal: str
+    msg: str
+    dia: int
 
 
 @app.get("/")
@@ -11,3 +17,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+@app.post("/doug/", response_model=Doug_Master)
+async def doug():
+    return Doug_Master(canal:="Douglas Canal", msg:"Sou o mestre do meu mundo!", dia:111)
+    
